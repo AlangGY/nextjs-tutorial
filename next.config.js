8f3f2d27bdd71b470d9f0aae1fd3aaf613a616ac
Alang Kim <rlaangh77@naver.com>
2022-02-10 02:12:13 +0900

    ci: create-next-app with typescript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
 /** @type {import('next').NextConfig} */
+
+const API_ENDPOINT = "https://api.themoviedb.org/3";
+const { API_KEY } = process.env;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "https://nomadcoders.co",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies/popular",
+        destination: `${API_ENDPOINT}/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `${API_ENDPOINT}/movie/:id?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
